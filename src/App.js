18380e133d96744e701960a910cf48eaf12dd95b
Fpu001 <127602677+Fpu001@users.noreply.github.com>
2023-10-02 13:38:42 +0300

    com2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+const API_URL = "https://random-data-api.com/api/coffee/random_coffee"
 
-function App() {
+function Coffee() {
+  const [blend_name, setBlendName] = useState('')
+  const [origin, setOrigin] = useState('')
+  const [intensifier, setIntensifier] = useState('')
+  useEffect(() => {
+  axios.get(API_URL)
+  .then((response)=> {
+    setBlendName(response.data.blend_name)
+    setOrigin(response.data.origin)
+    setIntensifier(response.data.intensifier)
+  }).catch((error)=> {
+    alert(error)
+  }) 
+}, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<h2>Coffee flavours</h2>
+<p>Name: {blend_name}</p>
+<p>Origin: {origin} </p>
+<p>intensifier: {intensifier}</p>
+</div> 
   );
 }
-
-export default App;
+export default Coffee; 
